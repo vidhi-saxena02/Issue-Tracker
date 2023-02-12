3446ae5c081f707fecb29017a90558bff103e9b0
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-02-13 01:07:35 +0530

    issue details header created

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,7 +1,26 @@
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import { IssueHeader } from "./IssueHeader";
+
+function useIssueData(issueNumber) {
+  return useQuery(["issues", issueNumber], () =>
+    fetch(`/api/issues/${issueNumber}`).then((res) => res.json())
+  );
+}
 
 export default function IssueDetails() {
   const { number } = useParams();
+  const issueQuery = useIssueData(number);
 
-  return <h1>Issue {number}</h1>;
+  return (
+    <div className="issue-details">
+      {issueQuery.isLoading ? (
+        <p>Loading Issue...</p>
+      ) : (
+        <>
+          <IssueHeader {...issueQuery.data} />
+        </>
+      )}
+    </div>
+  );
 }
