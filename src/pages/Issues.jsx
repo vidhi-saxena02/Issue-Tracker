6c2edcfb6c883d3676e43b98d456fb633cf835d0
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-02-13 00:24:07 +0530

    status filtering added

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,15 +1,17 @@
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
 import { useState } from "react";
+import { StatusSelect } from "../components/StatusSelect";
 
 export default function Issues() {
   const [label, setLabel] = useState([]);
+  const [status, setStatus] = useState("");
   return (
     <div>
       <main>
         <section>
           <h1>Issues</h1>
-          <IssuesList labels={label} />
+          <IssuesList labels={label} status={status} />
         </section>
         <aside>
           <LabelList
@@ -23,6 +25,11 @@ export default function Issues() {
                   : currentLabels.concat(label)
               )
             }
+          />
+          <h3>Status</h3>
+          <StatusSelect
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
           />
         </aside>
       </main>
