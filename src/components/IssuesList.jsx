5f8028f9c4c5125a945e37dc51973998da361249
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-02-13 02:34:25 +0530

    search functionality added

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,7 +1,9 @@
 import { useQuery } from "react-query";
 import { IssueItem } from "./IssueItem";
+import { useState } from "react";
 
 export default function IssuesList({ labels, status }) {
+  const [searchValue, setSearchValue] = useState("");
   const issuesQuery = useQuery(["issues", { labels, status }], () => {
     const statusString = status ? `&status=${status}` : "";
     const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
@@ -9,13 +11,40 @@ export default function IssuesList({ labels, status }) {
       res.json()
     );
   });
-  // console.log(issuesQuery.data);
+  const searchQuery = useQuery(
+    ["search", "issues", searchValue],
+    () =>
+      fetch(`/api/search/issues?q=${searchValue}`).then((res) => res.json()),
+    {
+      enabled: !!searchValue,
+    }
+  );
   return (
     <div>
-      <h2>Issues List</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setSearchValue(e.target.elements.search.value);
+        }}
+      >
+        <label htmlFor="search">Search</label>
+        <input
+          type="search"
+          placeholder="Search"
+          name="search"
+          id="search"
+          onChange={(e) => {
+            if (e.target.value.length === 0) {
+              setSearchValue("");
+            }
+          }}
+        />
+      </form>
+      <h2>Issue List</h2>
       {issuesQuery.isLoading ? (
         <p>Loading...</p>
-      ) : (
+      ) : searchQuery.fetchStatus === "idle" &&
+        searchQuery.isLoading === true ? (
         <ul className="issues-list">
           {issuesQuery.data.map((issue) => (
             <IssueItem
@@ -31,6 +60,32 @@ export default function IssuesList({ labels, status }) {
             />
           ))}
         </ul>
+      ) : (
+        <>
+          <h2>Search Results</h2>
+          {searchQuery.isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              <p>{searchQuery.data.count} Results</p>
+              <ul className="issues-list">
+                {searchQuery.data.items.map((issue) => (
+                  <IssueItem
+                    key={issue.id}
+                    title={issue.title}
+                    number={issue.number}
+                    assignee={issue.assignee}
+                    commentCount={issue.comments.length}
+                    createdBy={issue.createdBy}
+                    createdDate={issue.createdDate}
+                    labels={issue.labels}
+                    status={issue.status}
+                  />
+                ))}
+              </ul>
+            </>
+          )}
+        </>
       )}
     </div>
   );
